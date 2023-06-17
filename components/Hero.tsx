d86dd507bd2251f6d2f433db81c9d02faabf33c6
Hecater88 <47868066+Hecater88@users.noreply.github.com>
2023-06-17 17:06:00 +0200

    WIP

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
-	return <div>Hero</div>;
+	const handleScroll = () => {};
+	return (
+		<div className="hero">
+			<div className="flex-1 pt-36 padding-x">
+				<h1 className="hero__title">
+					Busca, reserva o alquila un coche — fácil y rápido
+				</h1>
+
+				<p className="hero__subtitle">
+					Agilice su experiencia de alquiler de coches con nuestro sencillo
+					proceso de reserva
+				</p>
+
+				<CustomButton
+					title="Explore Cars"
+					containerStyles="bg-primary-blue text-white rounded-full mt-10"
+					handleClick={handleScroll}
+				/>
+			</div>
+
+			<div className="hero__image-container">
+				<div className="hero__image">
+					<Image src="/hero.png" alt="hero" fill className="object-contain" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
