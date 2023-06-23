f689f0cffb04bf373e0aea2db3fc10e692487eb0
Hecater88 <47868066+Hecater88@users.noreply.github.com>
2023-06-23 18:13:08 +0200

    Car details done

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -39,7 +39,7 @@ const CarCard = ({ car }: CarCardProps) => {
 					className="object-contain"
 				/>
 			</div>
-			<div className="reñative flex w-full mt-2">
+			<div className="relative flex w-full mt-2">
 				<div className="flex group-hover:invisible w-full justify-between text-gray">
 					<div className="flex flex-col justify-center items-center gap-2">
 						<Image
